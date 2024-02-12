46a1d75a96f2cbe2a0d1de3501c921707eab27f5
Olexandr_Hramatovych <hramatovych.oleksandr@chnu.edu.ua>
2024-02-12 10:09:15 +0200

    redone task "int_within_bounds"

--- src/int_within_bounds.py
+++ src/int_within_bounds.py
@@ -1,5 +1,5 @@
 def int_within_bounds(number, lower_bound, upper_bound):
-  if number >= lower_bound and number < upper_bound:
+  if number >= lower_bound and number < upper_bound and type(number)==int:
     return True
   else:
     return False
